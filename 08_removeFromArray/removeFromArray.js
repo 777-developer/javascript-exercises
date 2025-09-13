289198b8f2f53160b1eec39bfec28f1cdd2b6df4
777-developer <212229513+777-developer@users.noreply.github.com>
2025-09-13 17:47:29 +0300

    modified: 02  08_removeFromArray js and spec.js files

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,13 +1,14 @@
 const removeFromArray = function(array, ...args) {
+   
     const newArray = [];
     
     array.forEach((item) => {
        if (!args.includes(item)) {
         newArray.push(item);
-       }
-       return newArray;
+       } 
     });
-
+    
+    return newArray;
 };
 
 

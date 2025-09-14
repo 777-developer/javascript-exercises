36017babec9e37baff7abf20c23a0da7345cc448
777-developer <212229513+777-developer@users.noreply.github.com>
2025-09-14 11:49:16 +0300

    modified:  0_leapYears/leapYears  js and spec.js files

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    const isYearsDivisibleByFour = year % 4 === 0;
+    const isCentury = year % 100 === 0;
+    const isYearsDivisibleByFourHundred = year % 400 === 0;
 
+    if (isYearsDivisibleByFour && (!isCentury || isYearsDivisibleByFourHundred)) {
+        return true;
+    } else {
+        return false;
+    }
+
+    return leapYears
 };
 
 // Do not edit below this line

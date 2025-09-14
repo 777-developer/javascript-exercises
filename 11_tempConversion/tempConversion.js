5ae1d6168bb1fac0a350d7515102bf4dd9dcc429
777-developer <212229513+777-developer@users.noreply.github.com>
2025-09-14 12:44:18 +0300

    11_tempConversion/tempConversion js && spec.js files

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  celsium = Math.round((fahrenheit - 32) * (5 / 9) * 10) / 10;
+  return celsium;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsium) {
+  fahrenheit = Math.round(((celsium * 9) / 5 + 32)*10) / 10;
+  return fahrenheit;
 };
 
 // Do not edit below this line
